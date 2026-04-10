1204ae86de4e11514d6d4b823a912e193b3dfcf8
lineo <lineo@lineodeMacBook-Air.local>
2026-04-10 22:58:18 +0800

    fix: 修复md渲染html样式丢失问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,26 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://lineo7387.github.io',
   base: '/Bauhaus-blog',
-  integrations: [mdx()],
+  integrations: [
+    mdx({
+      rehypePlugins: [
+        [
+          rehypePrettyCode,
+          {
+            theme: 'github-dark',
+            keepBackground: false,
+          },
+        ],
+      ],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
